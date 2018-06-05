3451856fd2774d2a5bad4682e9d972344563d5a0
Sergey Dubinin <whackojacko.ru@gmail.com>
2018-06-05 11:53:51 +0500

    TB-116: Implemented consistency check for object`s fields

--- src/server/meta/validation.go
+++ src/server/meta/validation.go
@@ -20,6 +20,11 @@ func (validationService *ValidationService) Validate(metaDescription *MetaDescri
 	if ok, err := validationService.checkFieldsDoesNotContainDuplicates(metaDescription.Fields); !ok {
 		return false, err
 	}
+	for _, field := range metaDescription.Fields {
+		if ok, err := validationService.checkFieldIsConsistent(field); !ok {
+			return false, err
+		}
+	}
 	return true, nil
 
 }
@@ -33,6 +38,13 @@ func (validationService *ValidationService) checkFieldsDoesNotContainDuplicates(
 		} else {
 			return false, &ValidationError{fmt.Sprintf("Object contains duplicated field '%s'", field.Name)}
 		}
+	}
+	return true, nil
+}
+
+func (validationService *ValidationService) checkFieldIsConsistent(field Field) (bool, error) {
+	if !field.Optional && field.Def != nil {
+		return false, &ValidationError{fmt.Sprintf("Mandatory field '%s' cannot have default value", field.Name)}
 	}
 	return true, nil
 }
