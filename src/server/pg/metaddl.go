38b59f96a1a41cebceff82d731da634e27030144
Sergey Dubinin <whackojacko.ru@gmail.com>
2018-06-13 11:36:06 +0500

    TB-126: Cleanup, removed unused constants

--- src/server/pg/metaddl.go
+++ src/server/pg/metaddl.go
@@ -338,7 +338,6 @@ func (md *MetaDDL) createTableScript() (*DDLStmt, error) {
 }
 
 //DDL drop table template
-const templDropTable95 = `DROP TABLE IF EXISTS "{{.Table}}";`
 const templDropTable94 = `DROP TABLE "{{.Table}}" {{.Mode}};`
 
 var parsedTemplDropTable = template.Must(template.New("drop_table").Funcs(ddlFuncs).Parse(templDropTable94))
@@ -474,7 +473,6 @@ func (fk *IFK) addScript(tname string) (*DDLStmt, error) {
 
 //DDL scripts to create sequence
 const templCreateSeq94 = `CREATE SEQUENCE "{{.Name}}";`
-const templCreateSeq95 = `CREATE SEQUENCE IF NOT EXISTS "{{.Name}}";`
 
 var parsedTemplCreateSeq = template.Must(template.New("add_seq").Funcs(ddlFuncs).Parse(templCreateSeq94))
 
@@ -488,7 +486,6 @@ func (s *Seq) createScript() (*DDLStmt, error) {
 
 //DDL scripts to drop sequence
 const templDropSeq94 = `DROP SEQUENCE "{{.Name}}";`
-const templDropSeq95 = `DROP SEQUENCE IF EXISTS "{{.Name}}";`
 
 var parsedTemplDropSeq = template.Must(template.New("drop_seq").Funcs(ddlFuncs).Parse(templDropSeq94))
 
@@ -548,7 +545,7 @@ type SliceCopyProcessor interface {
 // Id() is used as unique identifier in a set.
 // Second slice of m1 contains m1 / m2 difference, second slice of m2 contains m2 / m1 difference.
 func InverseIntersect(m1, m2 SliceCopyProcessor) {
-	var set map[string]int = make(map[string]int)
+	var set = make(map[string]int)
 	for i := 0; i < m1.Len(); i++ {
 		set[m1.Id(i)] = i
 	}
@@ -703,7 +700,7 @@ func (m *MetaDDLDiff) Script() (DDLStmts, error) {
 	return stmts, nil
 }
 
-const TableNamePrefix string = "o_"
+const TableNamePrefix = "o_"
 
 func tblName(m *meta.Meta) string {
 	name := bytes.NewBufferString(TableNamePrefix)
@@ -711,12 +708,8 @@ func tblName(m *meta.Meta) string {
 	return name.String()
 }
 
-func tblAlias(m *meta.Meta) string {
-	return string(m.Name[0])
-}
-
 func MetaDDLFromMeta(m *meta.Meta) (*MetaDDL, error) {
-	var metaDdl *MetaDDL = &MetaDDL{Table: tblName(m), Pk: m.Key.Name}
+	var metaDdl = &MetaDDL{Table: tblName(m), Pk: m.Key.Name}
 	metaDdl.Columns = make([]Column, 0, len(m.Fields))
 	metaDdl.IFKs = make([]IFK, 0, len(m.Fields)>>1)
 	metaDdl.OFKs = make([]OFK, 0, len(m.Fields)>>1)
@@ -760,7 +753,7 @@ func MetaDDLFromMeta(m *meta.Meta) (*MetaDDL, error) {
 	return metaDdl, nil
 }
 
-var seqNameParseRe *regexp.Regexp = regexp.MustCompile("nextval\\('(.*)'::regclass\\)")
+var seqNameParseRe = regexp.MustCompile("nextval\\('(.*)'::regclass\\)")
 
 func MetaDDLFromDB(tx *sql.Tx, name string) (*MetaDDL, error) {
 	md := &MetaDDL{Table: TableNamePrefix + name}
@@ -782,9 +775,4 @@ func MetaDDLFromDB(tx *sql.Tx, name string) (*MetaDDL, error) {
 	}
 
 	return md, nil
-}
-
-func (mddl *MetaDDL) create() {
-	//	var b bytes.Buffer
-	//	b.WriteString(")
 }
