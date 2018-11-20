2d0b4b2abdfbfc24ea7b3fdc2f7521d690f7c631
Sergey Dubinin <whackojacko.ru@gmail.com>
2018-11-20 13:40:31 +0500

    TB-192: Implemented AddFieldOperation ONLY for meta description-level

--- src/server/migrations/operations/field/add_field.go
+++ src/server/migrations/operations/field/add_field.go
@@ -4,6 +4,8 @@ import (
 	"server/object/description"
 	"server/object/meta"
 	"server/transactions"
+	"server/migrations"
+	"fmt"
 )
 
 type AddFieldOperation struct {
@@ -11,8 +13,31 @@ type AddFieldOperation struct {
 }
 
 func (o *AddFieldOperation) SyncMetaDescription(metaObj *meta.Meta, transaction transactions.MetaDescriptionTransaction, metaDescriptionSyncer meta.MetaDescriptionSyncer) (*meta.Meta, error) {
+	if err := o.validate(metaObj); err != nil {
+		return nil, err
+	}
+
 	metaDescription := metaObj.MetaDescription.Clone()
 	metaDescription.Fields = append(metaDescription.Fields, *o.Field)
-	//TODO: Implement processing of related fields(e.g. removal of outerLink on innerLink`s removal)
+
+	//factory new Meta
+	metaObj, err := new(meta.MetaFactory).FactoryMeta(metaDescription)
+	if err != nil {
+		return metaObj, nil
+	}
+	//sync its MetaDescription
+	if _, err = metaDescriptionSyncer.Update(metaObj.Name, *metaObj.MetaDescription); err != nil {
+		return nil, err
+	} else {
+		return metaObj, nil
+	}
+
 	return new(meta.MetaFactory).FactoryMeta(metaDescription)
+}
+
+func (o *AddFieldOperation) validate(metaObj *meta.Meta) error {
+	if metaObj.FindField(o.Field.Name) != nil {
+		return migrations.NewMigrationError(fmt.Sprintf("Object %s already has field %s", metaObj.Name, o.Field.Name))
+	}
+	return nil
 }
---
+++ src/server/migrations/operations/field/add_field_test.go
@@ -0,0 +1,91 @@
+package field
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"server/pg"
+	"utils"
+	"server/object/meta"
+	"server/transactions/file_transaction"
+	pg_transactions "server/pg/transactions"
+	"server/transactions"
+	"server/object/description"
+)
+
+var _ = Describe("'AddField' Migration Operation", func() {
+	appConfig := utils.GetConfig()
+	syncer, _ := pg.NewSyncer(appConfig.DbConnectionOptions)
+	metaDescriptionSyncer := meta.NewFileMetaDescriptionSyncer("./")
+	metaStore := meta.NewStore(metaDescriptionSyncer, syncer)
+
+	dataManager, _ := syncer.NewDataManager()
+	//transaction managers
+	fileMetaTransactionManager := &file_transaction.FileMetaDescriptionTransactionManager{}
+	dbTransactionManager := pg_transactions.NewPgDbTransactionManager(dataManager)
+	globalTransactionManager := transactions.NewGlobalTransactionManager(fileMetaTransactionManager, dbTransactionManager)
+
+	var globalTransaction *transactions.GlobalTransaction
+
+	var objectMeta *meta.Meta
+
+	//setup transaction
+	BeforeEach(func() {
+		var err error
+
+		globalTransaction, err = globalTransactionManager.BeginTransaction(nil)
+		Expect(err).To(BeNil())
+		metaStore.Flush(globalTransaction)
+		globalTransactionManager.CommitTransaction(globalTransaction)
+	})
+
+	//setup MetaDescription
+	BeforeEach(func() {
+		metaDescription := description.MetaDescription{
+			Name: "a",
+			Key:  "id",
+			Cas:  false,
+			Fields: []description.Field{
+				{
+					Name: "id",
+					Type: description.FieldTypeNumber,
+					Def: map[string]interface{}{
+						"func": "nextval",
+					},
+				},
+			},
+		}
+		var err error
+		objectMeta, err = new(meta.MetaFactory).FactoryMeta(&metaDescription)
+		Expect(err).To(BeNil())
+		err = metaDescriptionSyncer.Create(globalTransaction.MetaDescriptionTransaction, *objectMeta.MetaDescription)
+		Expect(err).To(BeNil())
+	})
+
+	//setup teardown
+	AfterEach(func() {
+		globalTransaction, err := globalTransactionManager.BeginTransaction(nil)
+		Expect(err).To(BeNil())
+
+		metaStore.Flush(globalTransaction)
+		globalTransactionManager.CommitTransaction(globalTransaction)
+	})
+
+	It("adds a field into metaDescription`s file", func() {
+
+		operation := AddFieldOperation{Field: &description.Field{Name: "new_field", Type: description.FieldTypeString, Optional: true}}
+		objectMeta, err := operation.SyncMetaDescription(objectMeta, globalTransaction.MetaDescriptionTransaction, metaDescriptionSyncer)
+		Expect(err).To(BeNil())
+		Expect(objectMeta).NotTo(BeNil())
+
+		//ensure Meta contains added field
+		Expect(objectMeta.FindField("new_field")).NotTo(BeNil())
+		//ensure MetaDescription has been save to file with new field
+		metaDescription, _, err := metaDescriptionSyncer.Get(objectMeta.Name)
+		Expect(metaDescription).NotTo(BeNil())
+		Expect(metaDescription.Fields).To(HaveLen(2))
+		Expect(metaDescription.Fields[1].Name).To(Equal("new_field"))
+
+		//clean up
+		metaDescriptionSyncer.Remove(metaDescription.Name)
+	})
+})
