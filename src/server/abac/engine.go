35b49d3057252d73e0e0ba0027a220d7d3a439cd
Alexander Gor <ornot.work@gmail.com>
2018-12-18 00:13:32 +0200

    T3-148. Abac engine stub

---
+++ src/server/abac/engine.go
@@ -0,0 +1,207 @@
+package abac
+
+import (
+	"fmt"
+	"strings"
+	"github.com/fatih/structs"
+	"reflect"
+)
+
+type Resolver interface {
+
+}
+
+type TroodABACResolver struct {
+	datasource map[string]interface{}
+}
+
+var operations map[string]func(interface{}, interface{})(bool, interface{})
+var aggregation map[string]func([]interface{}, interface{})(bool, interface{})
+
+func GetTroodABACResolver(datasource map[string]interface{}) TroodABACResolver {
+	operations = map[string]func(interface{}, interface{})(bool, interface{}) {
+		"eq": operatorExact,
+		"not": operatorNot,
+		"lt": operatorLt,
+		"gt": operatorGt,
+	}
+
+	aggregation = map[string]func([]interface{}, interface{})(bool, interface{}) {
+		"in": operatorIn,
+		"and": operatorAnd,
+		"or": operatorOr,
+	}
+
+	return TroodABACResolver{
+		datasource,
+	}
+}
+
+
+func (this *TroodABACResolver) EvaluateRule(rule map[string]interface{}) (bool, []string) {
+	condition := rule["rule"].(map[string]interface{})
+	result, filters := this.evaluateCondition(condition)
+
+	// todo: handle defaults and other edge cases
+
+	return result, filters
+}
+
+func (this *TroodABACResolver) evaluateCondition(condition map[string]interface{}) (bool, []string ) {
+	var filters []string
+
+	result := true
+	operator := "eq"
+
+	for operand, value := range condition {
+		switch value.(type) {
+		case map[string]interface{}:
+			for operator, value = range value.(map[string]interface{}) { break }
+		case []interface{}:
+			operator = operand
+		}
+
+		operand, value, is_filter := this.reveal(operand, value)
+
+		if is_filter {
+			f := makeFilter(operand.(string), value)
+			filters = append(filters, f)
+		} else {
+			if operator_func, ok := operations[operator]; ok {
+				result, _ = operator_func(value, operand)
+			}
+
+			if operator_func, ok := aggregation[operator]; ok {
+				if operator == "in" {
+					result, _ = operator_func(value.([]interface{}), operand)
+				} else {
+					result, _ = operator_func(value.([]interface{}), this)
+				}
+			}
+		}
+	}
+	return result, filters
+}
+
+func makeFilter(operand string, value interface{}) (string){
+	operator := "eq"
+
+	switch value.(type) {
+	case map[string]interface{}:
+		for operator, value = range value.(map[string]interface{}) { break }
+	}
+
+	return fmt.Sprint(operator, "(", operand, ",", value, ")")
+}
+
+func (this *TroodABACResolver) reveal(operand interface{}, value interface{}) (interface{}, interface{}, bool) {
+
+	var is_filter = false
+
+	splited := strings.SplitN(operand.(string), ".", 2)
+
+	if splited[0] == "obj" {
+		operand = splited[1]
+		is_filter = true
+	} else if splited[0] == "sbj" || splited[0] == "ctx" {
+		operand = GetAttributeByPath(this.datasource[splited[0]], splited[1])
+	}
+
+ 	if v, ok := value.(string); ok {
+		splited := strings.SplitN(v, ".", 2)
+		if splited[0] == "sbj" || splited[0] == "ctx" {
+			value = GetAttributeByPath(this.datasource[splited[0]], splited[1])
+		}
+	}
+
+	return operand, value, is_filter
+}
+
+func operatorExact(operand interface{}, value interface{}) (bool, interface{}) {
+	return cleanupType(operand) == cleanupType(value), nil
+}
+
+func operatorNot(operand interface{}, value interface{}) (bool, interface{}) {
+	return operand != value, nil
+}
+
+func operatorIn(value []interface{}, operand interface{}) (bool, interface{}) {
+	for _, v := range value {
+		if v == operand {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func operatorLt(value interface{}, operand interface{}) (bool, interface{}) {
+	f_value := value.(float64)
+	f_operand := operand.(float64)
+
+	return f_operand < f_value, nil
+}
+
+func operatorGt(value interface{}, operand interface{}) (bool, interface{}){
+	f_value := value.(float64)
+	f_operand := operand.(float64)
+
+	return f_operand > f_value, nil
+}
+
+func operatorAnd(value []interface{}, resolver interface{}) (bool, interface{}) {
+	var filters []string
+
+	for _, condition := range value {
+		r := resolver.(*TroodABACResolver)
+		res, flt := r.evaluateCondition(condition.(map[string]interface{}))
+		filters = append(filters, flt...)
+
+		if !res {
+			return false, nil
+		}
+	}
+	return true, filters
+}
+
+func operatorOr(value []interface{}, resolver interface{}) (bool, interface{}) {
+	var filters []string
+	for _, condition := range value {
+		r := resolver.(*TroodABACResolver)
+		res, flt := r.evaluateCondition(condition.(map[string]interface{}))
+		filters = append(filters, flt...)
+
+		if res {
+			return res, filters
+		}
+	}
+	return false, nil
+}
+
+func cleanupType(value interface{}) interface{}{
+	v := reflect.ValueOf(value)
+
+	floatType := reflect.TypeOf(float64(0))
+	if v.Type().ConvertibleTo(floatType) {
+		return v.Convert(floatType).Float()
+	}
+
+	return value
+}
+
+func GetAttributeByPath(obj interface{}, path string) interface{} {
+	attributes := strings.Split(path,".")
+	for _, key := range attributes {
+		switch obj.(type) {
+		case map[string]interface{}:
+			obj = obj.(map[string]interface{})[key]
+		case struct{}, interface{}:
+			structs.DefaultTagName = "json"
+			obj = structs.Map(obj)[key]
+		default:
+			return obj
+		}
+	}
+
+	return obj
+}
--- src/server/server.go
+++ src/server/server.go
@@ -8,6 +8,7 @@ import (
 	"server/object/meta"
 	"server/pg"
 	"server/auth"
+	"server/abac"
 	"github.com/julienschmidt/httprouter"
 	"io"
 	"mime"
@@ -26,6 +27,8 @@ import (
 	. "server/errors"
 	. "server/streams"
 	_ "net/http/pprof"
+	"fmt"
+	"os"
 )
 
 type CustodianApp struct {
@@ -54,6 +57,53 @@ func (app *CustodianApp) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if user, err := app.authenticator.Authenticate(req); err == nil {
 
 		ctx := context.WithValue(req.Context(), "auth_user", user)
+
+		resolver := abac.GetTroodABACResolver(map[string]interface{} {
+			"sbj": user,
+			"ctx": req,
+		},)
+
+		handled, opts, _ := app.router.Lookup(req.Method, req.RequestURI)
+
+		if handled != nil {
+			var res = opts.ByName("name")
+			var action = ""
+
+			splited := strings.Split(req.RequestURI, "/")
+
+			if res != "" {
+				if splited[2] == "meta" {
+					action = "meta_" + req.Method
+				} else if splited[2] == "data" {
+					action = "data_" + splited[3] + "_" + req.Method
+				}
+			} else {
+				if splited[2] == "meta" {
+					res = "meta"
+				} else {
+					res = "*"
+				}
+
+				action = req.Method
+			}
+
+			rules := abac.GetAttributeByPath(user.ABAC[os.Getenv("SERVICE_DOMAIN")], res + "." + action)
+
+			if rules != nil {
+				for _, rule := range rules.([]interface{}) {
+					fmt.Println(rule)
+					result, filters  := resolver.EvaluateRule(rule.(map[string]interface{}))
+
+					if result {
+						ctx = context.WithValue(ctx, "auth_filters", strings.Join(filters, ","))
+						break
+					} else {
+						returnError(w, auth.NewError("Access restricted by ABAC access rule"))
+						return
+					}
+				}
+			}
+		}
 
 		app.router.ServeHTTP(w, req.WithContext(ctx))
 	} else {
@@ -353,7 +403,17 @@ func (cs *CustodianServer) Setup(enableProfiler bool) *http.Server {
 					omitOuters = true
 				}
 
-				count, e = dataProcessor.GetBulk(dbTransaction, p.ByName("name"), pq.Get("q"), depth, omitOuters, func(obj map[string]interface{}) error { return sink.PourOff(obj) })
+				auth_filters := request.Context().Value("auth_filters");
+				user_filters := pq.Get("q")
+
+				var filters = ""
+				if auth_filters != nil && user_filters != "" {
+					filters = auth_filters.(string) + "," + user_filters
+				} else if user_filters != "" {
+					filters = user_filters
+				}
+
+				count, e = dataProcessor.GetBulk(dbTransaction, p.ByName("name"), filters , depth, omitOuters, func(obj map[string]interface{}) error { return sink.PourOff(obj) })
 				if e != nil {
 					sink.PushError(e)
 					dbTransactionManager.RollbackTransaction(dbTransaction)
