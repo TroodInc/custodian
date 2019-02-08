5a2ccee7abcf12c66a0904f6f2d5840fc85a2acd
Alexander Gor <ornot.work@gmail.com>
2019-02-08 10:54:35 +0200

    T3-672. rql tests

---
+++ src/server/pg/rql_test.go
@@ -0,0 +1,120 @@
+package pg
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"server/object/description"
+	"server/object/meta"
+	"utils"
+
+	"github.com/Q-CIS-DEV/go-rql-parser"
+
+	"server/data"
+
+	"strings"
+)
+
+var _ = Describe("RQL test", func(){
+	appConfig := utils.GetConfig()
+	syncer, _ := NewSyncer(appConfig.DbConnectionOptions)
+	metaStore := meta.NewStore(meta.NewFileMetaDescriptionSyncer("./"), syncer)
+
+	metaDescription := description.MetaDescription{
+		Name: "test",
+		Key:  "id",
+		Cas:  false,
+		Fields: []description.Field{
+			{
+				Name: "id",
+				Type: description.FieldTypeNumber,
+				Def: map[string]interface{}{
+					"func": "nextval",
+				},
+				Optional: true,
+			}, {
+				Name:     "test_field",
+				Type:     description.FieldTypeString,
+				Optional: true,
+			},
+		},
+	}
+	meta, _ := metaStore.NewMeta(&metaDescription)
+
+
+	dataNode := &data.Node{
+		KeyField:   meta.Key,
+		Meta:       meta,
+		ChildNodes: make(map[string]*data.Node),
+		Depth:      1,
+		OnlyLink:   false,
+		Parent:     nil,
+		Type:       data.NodeTypeRegular,
+	}
+
+	It("handle is_null() operator", func() {
+		Context("set to True", func() {
+			parser := rqlParser.NewParser()
+			rqlNode, _ := parser.Parse(strings.NewReader("is_null(test_field,true)"))
+			translator := NewSqlTranslator(rqlNode)
+
+			query, err := translator.query("test", dataNode)
+
+			Expect(err).To(BeNil())
+			Expect(query.Where).To(BeEquivalentTo("test.test_field IS NULL"))
+		})
+
+		Context("set to False", func() {
+			parser := rqlParser.NewParser()
+			rqlNode, _ := parser.Parse(strings.NewReader("is_null(test_field,false)"))
+			translator := NewSqlTranslator(rqlNode)
+
+			query, err := translator.query("test", dataNode)
+
+			Expect(err).To(BeNil())
+			Expect(query.Where).To(BeEquivalentTo("test.test_field IS NOT NULL"))
+		})
+
+		Context("other non Boolean string", func() {
+			parser := rqlParser.NewParser()
+			rqlNode, _ := parser.Parse(strings.NewReader("is_null(test_field,r4nd0m)"))
+			translator := NewSqlTranslator(rqlNode)
+
+			_, err := translator.query("test", dataNode)
+
+			Expect(err).To(Not(BeNil()))
+		})
+
+		Context("with additional rules", func() {
+			parser := rqlParser.NewParser()
+			rqlNode, _ := parser.Parse(strings.NewReader("eq(id,1),is_null(test_field,true),is_null(id,false)"))
+			translator := NewSqlTranslator(rqlNode)
+
+			query, err := translator.query("test", dataNode)
+
+			Expect(err).To(BeNil())
+			Expect(query.Where).To(BeEquivalentTo("(test.id =$1 AND test.test_field IS NULL AND test.id IS NOT NULL)"))
+		})
+	})
+
+	It("handle eq() operator", func() {
+		parser := rqlParser.NewParser()
+		rqlNode, _ := parser.Parse(strings.NewReader("eq(id,1)"))
+		translator := NewSqlTranslator(rqlNode)
+
+		query, err := translator.query("test", dataNode)
+
+		Expect(err).To(BeNil())
+		Expect(query.Where).To(BeEquivalentTo("test.id =$1"))
+	})
+
+	It("handle ne() operator", func() {
+		parser := rqlParser.NewParser()
+		rqlNode, _ := parser.Parse(strings.NewReader("ne(id,1)"))
+		translator := NewSqlTranslator(rqlNode)
+
+		query, err := translator.query("test", dataNode)
+
+		Expect(err).To(BeNil())
+		Expect(query.Where).To(BeEquivalentTo("test.id !=$1"))
+	})
+})
