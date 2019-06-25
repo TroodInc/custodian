25ec421331542143d1224ef40e35b44d6c2fa5cb
Alexander Gor <ornot.work@gmail.com>
2019-06-25 17:21:23 +0300

    RemoveMapAttributeByPath implementation

--- src/server/abac/utils.go
+++ src/server/abac/utils.go
@@ -22,19 +22,15 @@ func GetAttributeByPath(obj interface{}, path string) interface{} {
 	return obj
 }
 
-func RemoveMapAttributeByPath(obj interface{}, path string) map[string]interface{} {
+func RemoveMapAttributeByPath(obj map[string]interface{}, path string) map[string]interface{} {
 	attributes := strings.Split(path, ".")
 
-	for _, key := range attributes {
-		switch obj.(type) {
-		case map[string]interface{}:
-			obj = obj.(map[string]interface{})[key]
-		case struct{}, interface{}:
-			structs.DefaultTagName = "json"
-			obj = structs.Map(obj)[key]
-		default:
-			return obj
-		}
+	if len(attributes) == 1 {
+		delete(obj, attributes[0])
+		return obj
+	} else {
+		next := obj[attributes[0]].(map[string]interface{})
+		obj = RemoveMapAttributeByPath(next, strings.Join(attributes[1:], "."))
 	}
 
 	return obj
