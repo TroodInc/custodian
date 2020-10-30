c5b4557c2d2c117487e267262cccffe83f5f1f94
Kirill Pastukhov <kirush-ka@yandex.ru>
2020-10-30 14:53:05 +0300

    TB-552. Fix tests.

--- server/record_query_test.go
+++ server/record_query_test.go
@@ -232,37 +232,27 @@ var _ = Describe("Server", func() {
 			Expect(body["total_count"].(float64)).To(Equal(float64(20)))
 		})
 
-		It("returns records by query filtered using two separate parameters", func() {
+		It("returns same records using different queries", func() {
 			for i := 0; i < 20; i++ {
 				_, err := dataProcessor.CreateRecord(aMetaObj.Name, map[string]interface{}{"name": "A"}, auth.User{})
 				Expect(err).To(BeNil())
 			}
 
-			url := fmt.Sprintf("%s/data/%s?q=eq(name,A)&q=limit(0,5)", appConfig.UrlPrefix, aMetaObj.Name)
-
+			url := fmt.Sprintf("%s/data/%s?q=eq(name,A),gt(id,5),limit(0,10)", appConfig.UrlPrefix, aMetaObj.Name)
 			var request, _ = http.NewRequest("GET", url, nil)
 			httpServer.Handler.ServeHTTP(recorder, request)
 			responseBody := recorder.Body.String()
 
 			var body map[string]interface{}
 			json.Unmarshal([]byte(responseBody), &body)
-			Expect(body["data"].([]interface{})).To(HaveLen(5))
-			Expect(body["total_count"].(float64)).To(Equal(float64(20)))
-		})
+			Expect(body["data"].([]interface{})).To(HaveLen(10))
+			Expect(body["total_count"].(float64)).To(Equal(float64(15)))
 
-		It("returns records by query filtered using three separate parameters", func() {
-			for i := 0; i < 20; i++ {
-				_, err := dataProcessor.CreateRecord(aMetaObj.Name, map[string]interface{}{"name": "A"}, auth.User{})
-				Expect(err).To(BeNil())
-			}
-
-			url := fmt.Sprintf("%s/data/%s?q=eq(name,A)&q=gt(id,5)&q=limit(0,10)", appConfig.UrlPrefix, aMetaObj.Name)
-
-			var request, _ = http.NewRequest("GET", url, nil)
+			url = fmt.Sprintf("%s/data/%s?q=eq(name,A)&q=gt(id,5)&q=limit(0,10)", appConfig.UrlPrefix, aMetaObj.Name)
+			request, _ = http.NewRequest("GET", url, nil)
 			httpServer.Handler.ServeHTTP(recorder, request)
-			responseBody := recorder.Body.String()
+			responseBody = recorder.Body.String()
 
-			var body map[string]interface{}
 			json.Unmarshal([]byte(responseBody), &body)
 			Expect(body["data"].([]interface{})).To(HaveLen(10))
 			Expect(body["total_count"].(float64)).To(Equal(float64(15)))
