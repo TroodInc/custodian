4d7535763ffafec29d369dbb66d2967bf7b3e511
Alexander Gor <ornot.work@gmail.com>
2021-03-17 19:12:49 +0200

    unused MetaDescriptionTransactionManager removed

--- server/transactions/managers.go
+++ server/transactions/managers.go
@@ -1,15 +1,7 @@
 package transactions
 
-import "custodian/server/object/description"
-
 type DbTransactionManager interface {
 	BeginTransaction() (DbTransaction, error)
 	CommitTransaction(DbTransaction) error
 	RollbackTransaction(DbTransaction) error
-}
-
-type MetaDescriptionTransactionManager interface {
-	BeginTransaction(metaList []*description.MetaDescription) (MetaDescriptionTransaction, error)
-	CommitTransaction(MetaDescriptionTransaction) error
-	RollbackTransaction(MetaDescriptionTransaction) error
 }
