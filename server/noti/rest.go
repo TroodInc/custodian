c9ecc1a2c9225441fba93746e5892a96b6bb507a
Alexander Gor <ornot.work@gmail.com>
2021-07-14 14:41:47 +0300

    REST notifier cleanup

--- server/noti/rest.go
+++ server/noti/rest.go
@@ -2,12 +2,12 @@ package noti
 
 import (
 	"bytes"
-	"encoding/json"
 	"custodian/logger"
+	"custodian/server/auth"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/url"
-	"custodian/server/auth"
 	"time"
 )
 
@@ -38,59 +38,6 @@ func NewRestNotifier(args []string, activeIfNotRoot bool) (Notifier, error) {
 		return nil, NewNotiError(ErrRESTFailedURL, "Build a rest notifier failed. Specified URL '%s' is bad: %s", args[0], err.Error())
 	}
 	return &restNotifier{url: args[0], activeIfNotRoot: activeIfNotRoot}, nil
-}
-
-type jsonBodyStream struct {
-	w          io.WriteCloser
-	entityName string
-	empty      bool
-	status     string
-	err        []byte
-}
-
-func (jbs *jsonBodyStream) PourOff(o interface{}) error {
-	b, e := json.Marshal(o)
-	if e != nil {
-		return e
-	}
-
-	if jbs.empty {
-		jbs.empty = false
-		//use append to reduse count of chunks
-		_, e = jbs.w.Write(append([]byte("{\""+jbs.entityName+"\":["), b...))
-	} else {
-		_, e = jbs.w.Write(append([]byte(","), b...))
-	}
-	return e
-}
-
-func (jbs *jsonBodyStream) Failed(err error) {
-	//currently error not use but in the future it's possible
-	jbs.status = "FAILED"
-}
-
-func (jbs *jsonBodyStream) Complete() error {
-	defer func() {
-		jbs.w.Close()
-	}()
-	//use buffer to reduse count of chunks
-	var b bytes.Buffer
-	if jbs.empty {
-		jbs.empty = false
-		b.Write([]byte("{\"" + jbs.entityName + "\":["))
-	}
-	b.Write([]byte("],\"status\":\"" + jbs.status + "\""))
-	if jbs.err != nil {
-		b.Write([]byte(",\"error\":"))
-		b.Write(jbs.err)
-	}
-	b.Write([]byte("}"))
-	_, e := b.WriteTo(jbs.w)
-	return e
-}
-
-func newJsonBodyStream(entityName string, w io.WriteCloser) *jsonBodyStream {
-	return &jsonBodyStream{entityName: entityName, w: w, empty: true, status: "OK", err: nil}
 }
 
 type bufSilentWriter struct {
@@ -163,47 +110,12 @@ func (t *atLeastOneWriteCloser) Close() error {
 	return nil
 }
 
-func newAtLeastOneWriteCloser(writers ...io.WriteCloser) io.WriteCloser {
-	return &atLeastOneWriteCloser{writers}
-}
-
-func (rn *restNotifier) handle(event *Event, body *jsonBodyStream) error {
-	if event.err != nil {
-		body.Failed(event.err)
-		return event.err
-	}
-	if rn.activeIfNotRoot || event.isRoot  {
-		return body.PourOff(event.obj)
-	}
-	return nil
-}
-
-//must be fast bacause of there is backprebackpre to object operations
-func (rn *restNotifier) consume(in chan *Event, body *jsonBodyStream, complete chan bool) {
-	defer func() {
-		body.Complete()
-		complete <- true
-	}()
-	handle := rn.handle
-	var err error
-	for event := range in {
-		if err = handle(event, body); err != nil {
-			logger.Error("The notification has stopped due to en error: %s", err.Error())
-			handle = func(_ *Event, _ *jsonBodyStream) error { return nil }
-		}
-	}
-}
-
 func (rn *restNotifier) redelivery(body []byte, attempt byte) {
 	timer := time.NewTimer(time.Second * REST_REDELIVERY_PAUSE_IN_SEC)
 	logger.Info("Scheduled '%d' attempt of re-delivery notification for '%s' URL in '%d' seconds", attempt, rn.url, REST_REDELIVERY_PAUSE_IN_SEC)
 	<-timer.C
 	logger.Info("Setup '%d' attempt of rre-delivery notification for '%s' URL", attempt, rn.url)
-	pr, pw := io.Pipe()
-	go func() {
-		pw.Write(body)
-		pw.Close()
-	}()
+
 	tryAgain := func() {
 		if attempt < REST_MAX_REDILIVERY_ATTEMPTS {
 			go rn.redelivery(body, attempt+1)
@@ -211,7 +123,7 @@ func (rn *restNotifier) redelivery(body []byte, attempt byte) {
 			logger.Error("Can't schedule re-delivery for '%s' URL. Achived max re-delivery attempts '%d'", rn.url, REST_MAX_REDILIVERY_ATTEMPTS)
 		}
 	}
-	resp, err := postCallbackData(rn.url, pr)
+	resp, err := postCallbackData(rn.url, bytes.NewReader(body))
 	if err != nil {
 		logger.Error("Error sending notification: %s", err.Error())
 		tryAgain()
@@ -225,31 +137,25 @@ func (rn *restNotifier) redelivery(body []byte, attempt byte) {
 }
 
 func (rn *restNotifier) start(in chan *Event) {
-	pr, pw := io.Pipe()
-	redelBuf := newBufSilentWriter(REST_MAX_REDELIVERY_BUF_SIZE)
-	mw := newAtLeastOneWriteCloser(pw, redelBuf)
-	body := newJsonBodyStream("events", mw)
-	complete := make(chan bool, 1)
-	go rn.consume(in, body, complete)
-	resp, err := postCallbackData(rn.url, pr)
-	failed := false
-	if err != nil {
-		logger.Error("Error sending notification: %s", err.Error())
-		failed = true
-	} else {
-		resp.Body.Close()
-		if resp.StatusCode != 200 {
-			logger.Error("Received an invalid response code '%d' from the '%s' notified server", resp.StatusCode, rn.url)
-			failed = true
-		}
-	}
+	for event := range in {
+		body, _ := json.Marshal(event.Obj())
+		resp, err := postCallbackData(rn.url, bytes.NewReader(body))
 
-	//waiting for the end of cunsumption
-	if <-complete && failed {
-		if !redelBuf.isExceeded() {
-			go rn.redelivery(redelBuf.buf.Bytes(), 1)
-		} else if REST_MAX_REDELIVERY_BUF_SIZE > 0 {
-			logger.Error("Can't schedule re-delivery for '%s' URL. The body size '%d' is exceeded specified max redilivery buffer size '%d'", rn.url, redelBuf.total, REST_MAX_REDELIVERY_BUF_SIZE)
+		failed := false
+		if err != nil {
+			logger.Error("Error sending notification: %s", err.Error())
+			failed = true
+		} else {
+			resp.Body.Close()
+			if resp.StatusCode != 200 {
+				logger.Error("Received an invalid response code '%d' from the '%s' notified server", resp.StatusCode, rn.url)
+				failed = true
+			}
+		}
+
+		//waiting for the end of cunsumption
+		if failed {
+			go rn.redelivery(body, 1)
 		}
 	}
 }
@@ -262,14 +168,14 @@ func (rn *restNotifier) NewNotification() chan *Event {
 }
 
 func postCallbackData(url string, body io.Reader) (*http.Response, error){
-	callback_request, _ := http.NewRequest("POST", url, body)
-	callback_request.Header.Add("Content-Type", "application/json")
+	callbackRequest, _ := http.NewRequest("POST", url, body)
+	callbackRequest.Header.Add("Content-Type", "application/json")
 
-	service_token, err := auth.GetServiceToken()
+	serviceToken, err := auth.GetServiceToken()
 
 	if err == nil {
-		callback_request.Header.Add("Authorization", "Service " + service_token)
+		callbackRequest.Header.Add("Authorization", "Service " +serviceToken)
 	}
 
-	return restClient.Do(callback_request)
+	return restClient.Do(callbackRequest)
 }
